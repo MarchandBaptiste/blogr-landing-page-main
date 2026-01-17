35c47fe3bdeea3f1144a8cb00ecff6f311dda412
Marchand <baptistemarchand2007@gmail.com>
2026-01-17 23:12:49 +0100

    version mobile

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+// Test si l'élément existe
+const btnMenu = document.getElementById("menu-toggle");
+const body = document.body;
+
+btnMenu.addEventListener("click", () => {
+  body.classList.toggle("menu-open");
+  if (body.classList.contains("menu-open")) {
+    btnMenu.src = "../assets/images/icon-close.svg";
+  } else {
+    btnMenu.src = "../assets/images/icon-hamburger.svg";
+  }
+});
+
+// Toggle des sous-menus
+const menuSections = document.querySelectorAll(".menu-section");
+
+menuSections.forEach((section) => {
+  const menuTitle = section.querySelector(".menu-title");
+  if (menuTitle) {
+    menuTitle.addEventListener("click", () => {
+      section.classList.toggle("active");
+    });
+  }
+});
